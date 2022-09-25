fc1cba861263b117a45f61a7f4e0f1c3a847d310
Adil Heybetov <heybetov1998@gmail.com>
2022-09-25 12:15:49 +0400

    topBarbers section routing completed

--- src/components/Home/TopBarbers/BarberSlide.tsx
+++ src/components/Home/TopBarbers/BarberSlide.tsx
@@ -4,16 +4,17 @@ interface Props {
     imageSrc: string;
     name: string;
     rating: number;
+    barberId:string;
 }
 
 const BarberSlide = (props: Props) => {
     return (
         <div className="barber_slide">
-            <Link to={"#"} className="image_container">
+            <Link to={`/barbers/${props.barberId}`} className="image_container">
                 <img src={props.imageSrc} alt="barber profile" />
             </Link>
             <div>
-                <Link to={"#"} className="barber_name">
+                <Link to={`/barbers/${props.barberId}`} className="barber_name">
                     {props.name}
                 </Link>
                 <p>Rating: {props.rating}</p>
