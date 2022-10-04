07a841c27e9fbc3d74f303dc34ab172bf4e73a3c
Adil Heybetov <heybetov1998@gmail.com>
2022-10-04 21:51:38 +0400

    not found page is ready

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,5 +1,17 @@
+import { Link } from "react-router-dom";
+
 const NotFound = () => {
-    return <div>not found</div>;
+    return (
+        <section
+            id="notFound"
+            className="d-flex flex-column justify-content-center align-items-center vh-100"
+        >
+            <p className="mb-3 text">It is time to go home</p>
+            <Link className="text-white" to="/">
+                Go Home
+            </Link>
+        </section>
+    );
 };
 
 export default NotFound;
